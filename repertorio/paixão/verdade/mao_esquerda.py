de44d174c5b1928b441dd8064ef2b5137af6bcc5
Breder <c.brederfigueiredo@gmail.com>
2024-10-04 16:12:45 -0300

    04-10

--- repertorio/paixão/verdade/mao_esquerda.py
+++ repertorio/paixão/verdade/mao_esquerda.py
@@ -14,7 +14,7 @@ serialString = ''
 
 midiout = rtmidi.MidiOut()
 print(midiout.get_ports())
-port = midiout.open_port(3)
+port = midiout.open_port(1)
 
 with open('mapNotas.json') as jsonfile:
       mapNotas = json.load(jsonfile)
